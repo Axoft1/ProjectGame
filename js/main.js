71179f6411a1b31f84777cc3bb5e9e8c0fb2a7d1
Aleksei Anufriev <axoft10@gmail.com>
2022-07-24 10:24:51 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,55 @@
+const buttonAuth = document.querySelector('.button-auth')
+const modalAuth = document.querySelector('.modal-auth')
+const buttonOut = document.querySelector('.button-out')
+const userName = document.querySelector('.user-name')
+const closeAuth = document.querySelector('.close-auth')
+const logInForm = document.getElementById('logInForm')
+const inputLogin = document.getElementById('login')
+const inputPassword = document.getElementById('password')
+
+
+
+const login = (user) => {
+    buttonAuth.style.display = 'none'
+
+    buttonOut.style.display = 'block'
+    userName.style.display = 'block'
+
+    userName.textContent = user.login
+    modalAuth.style.display = 'none'
+
+}
+const loginOut = () => {
+    buttonAuth.style.display = 'flex'
+
+    buttonOut.style.display = 'none'
+    userName.style.display = 'none'
+    userName.textContent = ''
+
+    localStorage.removeItem('user')
+}
+
+
+buttonAuth.addEventListener('click', () => {
+    modalAuth.style.display = 'flex'
+})
+closeAuth.addEventListener('click', () => {
+    modalAuth.style.display = 'none'
+})
+buttonOut.addEventListener('click', () => {
+    loginOut()
+})
+logInForm.addEventListener('submit', (event) => {
+    event.preventDefault()// блокировка стадартного поведения
+
+    const user = {
+        login: inputLogin.value,
+        password: inputPassword.value
+    }
+    localStorage.setItem('user', JSON.stringify(user))
+    login(user)
+})
+
+if(localStorage.getItem('user')){
+    login(JSON.parse(localStorage.getItem(user)))
+}
